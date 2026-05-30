a70d0fcc30d6013f64df50d17e029244d23df65e
Veli-Pekka Luoto <jullesammakka@hotmail.fi>
2026-05-30 21:38:33 +0300

    as1

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,12 +1,17 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 
+function clickRandom() {
+  return Math.floor(Math.random()*100);
+}
+
 @Component({
   selector: 'app-root',
   imports: [RouterOutlet],
   templateUrl: './app.html',
-  styleUrl: './app.css'
+  styleUrls: ['./app.css']
 })
 export class App {
-  protected readonly title = signal('angular_event_handling');
+  protected readonly title = signal('Click Button For Random Number!');
+  protected readonly clickRandom = clickRandom;
 }
